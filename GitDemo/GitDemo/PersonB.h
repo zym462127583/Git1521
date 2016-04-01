c1a3a41c1376539a24713f1738ff60906fb4b009
qianfeng <qianfeng@depiaodepiaodeyidepiao.local>
2016-04-01 15:05:49 +0800

    B创建一个PersonB类

---
+++ GitDemo/GitDemo/PersonB.h
@@ -0,0 +1,13 @@
+//
+//  PersonB.h
+//  GitDemo
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 ZYM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PersonB : NSObject
+
+@end
