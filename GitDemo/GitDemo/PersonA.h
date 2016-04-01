d4be1d150f1e24bb5059619884fd7f5465a8777c
qianfeng <qianfeng@depiaodepiaodeyidepiao.local>
2016-04-01 15:10:21 +0800

    A创建一个PersonA类

---
+++ GitDemo/GitDemo/PersonA.h
@@ -0,0 +1,13 @@
+//
+//  PersonA.h
+//  GitDemo
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 ZYM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PersonA : NSObject
+
+@end
---
+++ GitDemo/GitDemo/PersonA.m
@@ -0,0 +1,13 @@
+//
+//  PersonA.m
+//  GitDemo
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 ZYM. All rights reserved.
+//
+
+#import "PersonA.h"
+
+@implementation PersonA
+
+@end
